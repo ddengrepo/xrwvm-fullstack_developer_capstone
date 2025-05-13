ebe68e6aa4af10415ec631e7200a331ea8666c86
ddengrepo <ddeng.repo@gmail.com>
2025-05-12 22:58:16 -0400

    Lint:v7

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -34,12 +34,12 @@
                 or application.
 """
 
-#from django.http import HttpResponseRedirect, HttpResponse
+# from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-#from django.shortcuts import get_object_or_404, render, redirect
+# from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-#from django.contrib import messages
-from datetime import datetime
+# from django.contrib import messages
+# from datetime import datetime
 
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
@@ -115,6 +115,8 @@ def logout_request(request):
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
 def registration(request):
+    # context = {}
+    # load & extract request data to use in below logic
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
@@ -122,7 +124,7 @@ def registration(request):
     last_name = data['lastName']
     email = data['email']
     username_exist = False
-    #email_exist = False
+    # email_exist = False
 
     try:
         # Check if user already exists with username
@@ -201,6 +203,7 @@ def get_dealer_reviews(request, dealer_id):
                 - "message" (str, if status is 400): An error message
                         indicating a bad request.
     """
+    # dealer = get_object_or_404(Dealer, pk=dealer_id)
     if (dealer_id):
         endpoint = "/fetchReviews/dealer/"+str(dealer_id)
         reviews = get_request(endpoint)
@@ -234,6 +237,7 @@ def get_dealer_details(request, dealer_id):
                 - "message" (str, if status is 400):
                         An error message indicating a bad request.
     """
+    # dealer_id = get_object_or_404(Dealer, pk=dealer_id)
     if (dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
